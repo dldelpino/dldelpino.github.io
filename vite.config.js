c4960ed2eb159f4e4bd77f0e440e07f6968ebbc5
David <lopezdelpinodavid@gmail.com>
2025-05-21 18:34:26 +0200

    GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/dldelpino.github.io",
+  base: "/dldelpino.github.io/",
   plugins: [
     react(),
     tailwindcss()
